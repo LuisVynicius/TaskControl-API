be2c02fa4bca810789bad8c217c6a70ea11155da
LuisVynicius <luismevy@gmail.com>
2024-07-06 22:24:59 -0300

    Created Project

---
+++ src/main/java/com/mevy/taskcontrolapi/TaskcontrolapiApplication.java
@@ -0,0 +1,13 @@
+package com.mevy.taskcontrolapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskcontrolapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskcontrolapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mevy/taskcontrolapi/TaskcontrolapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mevy.taskcontrolapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskcontrolapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
